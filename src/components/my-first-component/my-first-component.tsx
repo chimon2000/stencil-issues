4ad9c5680dfdee4690f8372a5b0ea9baabf93e4d
Ryan <ryan.edge@zoho.com>
2017-08-24 12:01:01 -0400

    Add issue code

--- src/components/my-first-component/my-first-component.tsx
+++ src/components/my-first-component/my-first-component.tsx
@@ -1,9 +1,22 @@
 import { Component } from '@stencil/core'
+import Cleave from 'cleave.js'
 
 @Component({
     tag: 'my-first-component'
 })
 export class MyComponent {
+    // inputRef: HTMLElement
+
+    get inputRef() {
+        return this['__el'].querySelector('input')
+    }
+    componentDidLoad() {
+        new Cleave(this.inputRef, {
+            delimiter: '-',
+            blocks: [3, 2, 4]
+        })
+    }
+
     render() {
         return (
             <p>
